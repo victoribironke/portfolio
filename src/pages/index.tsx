e8206a69123e4c241b7c2d4095da040596ba2f31
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-21 22:01:13 +0100

    added seo image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,39 @@ const Home = () => {
         <link rel="icon" href={src} type="image/x-icon" />
         <meta
           name="description"
-          content="victor ibironke is an enthusiastic full stack developer"
+          content="victor ibironke is a full stack developer with an ethusiastic drive for building high-quality web projects."
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://www.victoribironke.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Victor Ibironke ~ full stack developer"
+        />
+        <meta
+          property="og:description"
+          content="victor ibironke is a full stack developer with an ethusiastic drive for building high-quality web projects."
+        />
+        <meta
+          property="og:image"
+          content="https://www.victoribironke.com/og-image.png"
+        />
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="www.victoribironke.com" />
+        <meta property="twitter:url" content="https://www.victoribironke.com" />
+        <meta
+          name="twitter:title"
+          content="Victor Ibironke ~ full stack developer"
+        />
+        <meta
+          name="twitter:description"
+          content="victor ibironke is a full stack developer with an ethusiastic drive for building high-quality web projects."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.victoribironke.com/og-image.png"
         />
       </Head>
 
@@ -40,11 +72,6 @@ const Home = () => {
       <Projects />
       <ContributionGraph />
       <Contact />
-
-      {/* work on an images for seo for when your link on social media */}
-      {/* work experience-victor eke
-projects-victor eke
- */}
     </>
   );
 };
