ee77e05e5c54ebb79e57c380925412e3175c15cd
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-23 11:47:07 +0100

    updated contact links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -26,20 +26,28 @@ const Contact = () => {
       <p className="w-fit max-w-5xl text-left font-mono text-blue bg-gray-100 bg-opacity-5 px-6 py-2 self-start backdrop-blur-sm rounded-xl absolute -top-12 left-0 z-0">
         contact
       </p>
-      <div className="w-full lg:w-3/5 bg-gray-100 bg-opacity-5 p-6 backdrop-blur-sm rounded-2xl flex flex-col gap-4">
-        <p className="text-xl max-w-xl md:max-w-2xl z-10">
-          Want to get in touch?
+
+      <div className="w-full lg:w-3/5 bg-gray-100 bg-opacity-5 p-6 backdrop-blur-sm rounded-2xl flex flex-col gap-2">
+        <p className="text-lg max-w-xl md:max-w-2xl z-10">
+          Want to get in touch? Shoot me a message!
         </p>
 
-        <p className="text-lg md:text-xl max-w-2xl z-10">
-          Shoot me an email @{" "}
+        <div className="w-full md:w-[50%] flex items-center gap-6 z-10">
           <Link
             href="mailto:victoribironke2019@gmail.com"
-            className="text-blue"
+            target="_blank"
+            className="text-blue border-b border-blue hover:px-1"
           >
-            victoribironke2019@gmail.com
+            Mail
           </Link>
-        </p>
+          <Link
+            href="https://dm.new/victor"
+            target="_blank"
+            className="text-blue border-b border-blue hover:px-1"
+          >
+            Direct message
+          </Link>
+        </div>
       </div>
       <div className="w-full lg:w-2/5 grid grid-cols-2 grid-rows-2 gap-4">
         {footerLinks.map((f, i) => (
@@ -47,7 +55,7 @@ const Contact = () => {
             href={f.url}
             target="_blank"
             key={i}
-            className="w-full flex gap-4 rounded-2xl bg-gray-100 bg-opacity-5 p-4 md:p-6 backdrop-blur-sm border border-transparent hover:border-zinc-700 text-sm md:text-base"
+            className="w-full flex items-center gap-4 rounded-2xl bg-gray-100 bg-opacity-5 p-4 md:p-6 backdrop-blur-sm border border-transparent hover:border-zinc-700 text-sm md:text-base"
           >
             <f.icon />
             {f.text}
