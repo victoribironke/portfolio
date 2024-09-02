249e28ecdd4ad579046da43543b6d47849a9d5f6
Victor Ibironke <victoribironke2019@gmail.com>
2024-09-02 21:47:06 +0100

    fix: fixed seo bug (title)

--- src/components/general/HeadTemplate.tsx
+++ src/components/general/HeadTemplate.tsx
@@ -11,14 +11,14 @@ const HeadTemplate = ({ meta }: { meta: Meta }) => {
       {/* <!-- Facebook Meta Tags --> */}
       <meta property="og:url" content={meta.url} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="BCW" />
+      <meta property="og:title" content={meta.title} />
       <meta property="og:description" content={meta.description} />
       <meta property="og:image" content={meta.og_image} />
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={meta.url.split("https://")[0]} />
       <meta property="twitter:url" content={meta.url} />
-      <meta name="twitter:title" content="BCW" />
+      <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.og_image} />
     </Head>
