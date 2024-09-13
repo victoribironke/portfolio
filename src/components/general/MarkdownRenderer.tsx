4042c2d34f6ea04ae1c2bdec7fd739c94c84ba3d
Victor Ibironke <victoribironke2019@gmail.com>
2024-09-13 10:50:34 +0100

    fix: changed code formatter

--- src/components/general/MarkdownRenderer.tsx
+++ src/components/general/MarkdownRenderer.tsx
@@ -1,30 +1,12 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-// import { vscDarkPlus,funky } from "react-syntax-highlighter/dist/esm/styles/prism"; // You can choose a different style
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github.css";
 
 const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
   return (
     <ReactMarkdown
-      components={{
-        code: ({ inline, className, children, ...props }) => {
-          const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
-            <SyntaxHighlighter
-              // style={funky}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
+      rehypePlugins={[rehypeHighlight]}
       className="markdown font-sub"
     >
       {markdown}
