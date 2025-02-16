3235a2629589bf916bf152f4acbbed3ab70ae103
Victor ⋈ <82767685+victoribironke@users.noreply.github.com>
2025-02-16 10:07:26 +0100

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
     <>
       <Separator />
 
-      <div className="w-full text-lg flex items-center justify-center gap-4 flex-wrap">
+      <div className="w-full md:text-lg flex items-center justify-center gap-4 flex-wrap">
         <Link
           href={PAGES.github}
           target="_blank"
-          className="font-light md-text-lg border-b border-dashed w-fit hover:border-b-blue"
+          className="font-light md:text-lg border-b border-dashed w-fit hover:border-b-blue"
         >
           github
         </Link>
@@ -21,7 +21,7 @@ const Footer = () => {
         <Link
           href={PAGES.twitter}
           target="_blank"
-          className="font-light md-text-lg border-b border-dashed w-fit hover:border-b-blue"
+          className="font-light md:text-lg border-b border-dashed w-fit hover:border-b-blue"
         >
           twitter
         </Link>
@@ -31,7 +31,7 @@ const Footer = () => {
         <Link
           href={PAGES.instagram}
           target="_blank"
-          className="font-light md-text-lg border-b border-dashed w-fit hover:border-b-blue"
+          className="font-light md:text-lg border-b border-dashed w-fit hover:border-b-blue"
         >
           instagram
         </Link>
