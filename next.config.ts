4302a577d40d5557ef31a1a01a24846eb89499c2
Victor Ibironke <victoribironke2019@gmail.com>
2026-05-14 22:46:37 +0100

    feat: add i.scdn.co to allowed image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.sanity.io",
       },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
     ],
   },
   // headers: async () => {
