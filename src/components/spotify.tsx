90334e864f8574c4b874a606f2eb61862cd462e5
Victor Ibironke <victoribironke2019@gmail.com>
2026-05-14 21:33:32 +0100

    feat: add new Spotify component in src/components/spotify.tsx

---
+++ src/components/spotify.tsx
@@ -0,0 +1,21 @@
+const Spotify = () => {
+  // .item-card {
+  //   padding: 1rem 1.25rem;
+  //   border-radius: 0.75rem;
+  //   border: 1px solid transparent;
+  //   transition: all 250ms ease;
+  // }
+
+  // .item-card:hover {
+  //   background: hsla(240, 6%, 12%, 0.5);
+  //   border-color: hsla(240, 5%, 22%, 0.7);
+  // }
+
+  return (
+    <div className="p-4 rounded-[0.75rem] border border-[#35353bb3] bg-[#1d1d2080]">
+      Spotify
+    </div>
+  );
+};
+
+export default Spotify;
